62332c72ca7fb4790c0b33665a9bb510b8c91541
Artsiom Mezin <iketari@gmail.com>
2017-12-08 18:49:17 +0000

    README.md

--- components/chat/chat.jade.js
+++ components/chat/chat.jade.js
@@ -5,7 +5,7 @@ function pug_classes_object(r){var a="",n="";for(var o in r)o&&r[o]&&pug_has_own
 function pug_escape(e){var a=""+e,t=pug_match_html.exec(a);if(!t)return e;var r,c,n,s="";for(r=t.index,c=0;r<a.length;r++){switch(a.charCodeAt(r)){case 34:n="&quot;";break;case 38:n="&amp;";break;case 60:n="&lt;";break;case 62:n="&gt;";break;default:continue}c!==r&&(s+=a.substring(c,r)),c=r+1,s+=n}return c!==r?s+a.substring(c,r):s}
 var pug_has_own_property=Object.prototype.hasOwnProperty;
 var pug_match_html=/["&<>]/;
-function pug_rethrow(n,e,r,t){if(!(n instanceof Error))throw n;if(!("undefined"==typeof window&&e||t))throw n.message+=" on line "+r,n;try{t=t||require("fs").readFileSync(e,"utf8")}catch(e){pug_rethrow(n,null,r)}var i=3,a=t.split("\n"),o=Math.max(r-i,0),h=Math.min(a.length,r+i),i=a.slice(o,h).map(function(n,e){var t=e+o+1;return(t==r?"  > ":"    ")+t+"| "+n}).join("\n");throw n.path=e,n.message=(e||"Pug")+":"+r+"\n"+i+"\n\n"+n.message,n}function chatTemplate(locals) {var pug_html = "", pug_mixins = {}, pug_interp;var pug_debug_filename, pug_debug_line;try {var pug_debug_sources = {"components\u002Fchat\u002Fchat.jade":"div.chat\n    div.chat__container\n        div.header\n            h2 You are logged in as %username%\n        div.chat__box\n            each message in messages\n                div(class=\"message-box \" + (message.isMine ? \"right-img\" : \"left-img\"))\n                    div.picture\n                        img(src= message.avatar)\n                        span.time 21:30\n                    div.message\n                        span=message.name\n                        p=message.text\n"};
+function pug_rethrow(n,e,r,t){if(!(n instanceof Error))throw n;if(!("undefined"==typeof window&&e||t))throw n.message+=" on line "+r,n;try{t=t||require("fs").readFileSync(e,"utf8")}catch(e){pug_rethrow(n,null,r)}var i=3,a=t.split("\n"),o=Math.max(r-i,0),h=Math.min(a.length,r+i),i=a.slice(o,h).map(function(n,e){var t=e+o+1;return(t==r?"  > ":"    ")+t+"| "+n}).join("\n");throw n.path=e,n.message=(e||"Pug")+":"+r+"\n"+i+"\n\n"+n.message,n}function chatTemplate(locals) {var pug_html = "", pug_mixins = {}, pug_interp;var pug_debug_filename, pug_debug_line;try {var pug_debug_sources = {"components\u002Fchat\u002Fchat.jade":"div.chat\n    div.chat__container\n        div.header\n            h2 You are logged in as %username%\n        div.chat__box\n            each message in messages\n                div(class=\"message-box \" + (message.isMine ? \"right-img\" : \"left-img\"))\n                    div.picture\n                        img(src= message.avatar)\n                        span.time= message.date.getHours() + ':' + message.date.getMinutes()\n                    div.message\n                        span=message.name\n                        p=message.text\n"};
 ;var locals_for_with = (locals || {});(function (messages) {;pug_debug_line = 1;pug_debug_filename = "components\u002Fchat\u002Fchat.jade";
 pug_html = pug_html + "\u003Cdiv class=\"chat\"\u003E";
 ;pug_debug_line = 2;pug_debug_filename = "components\u002Fchat\u002Fchat.jade";
@@ -34,7 +34,7 @@ pug_html = pug_html + "\u003Cimg" + (pug_attr("src", message.avatar, true, false
 ;pug_debug_line = 10;pug_debug_filename = "components\u002Fchat\u002Fchat.jade";
 pug_html = pug_html + "\u003Cspan class=\"time\"\u003E";
 ;pug_debug_line = 10;pug_debug_filename = "components\u002Fchat\u002Fchat.jade";
-pug_html = pug_html + "21:30\u003C\u002Fspan\u003E\u003C\u002Fdiv\u003E";
+pug_html = pug_html + (pug_escape(null == (pug_interp = message.date.getHours() + ':' + message.date.getMinutes()) ? "" : pug_interp)) + "\u003C\u002Fspan\u003E\u003C\u002Fdiv\u003E";
 ;pug_debug_line = 11;pug_debug_filename = "components\u002Fchat\u002Fchat.jade";
 pug_html = pug_html + "\u003Cdiv class=\"message\"\u003E";
 ;pug_debug_line = 12;pug_debug_filename = "components\u002Fchat\u002Fchat.jade";
@@ -60,7 +60,7 @@ pug_html = pug_html + "\u003Cimg" + (pug_attr("src", message.avatar, true, false
 ;pug_debug_line = 10;pug_debug_filename = "components\u002Fchat\u002Fchat.jade";
 pug_html = pug_html + "\u003Cspan class=\"time\"\u003E";
 ;pug_debug_line = 10;pug_debug_filename = "components\u002Fchat\u002Fchat.jade";
-pug_html = pug_html + "21:30\u003C\u002Fspan\u003E\u003C\u002Fdiv\u003E";
+pug_html = pug_html + (pug_escape(null == (pug_interp = message.date.getHours() + ':' + message.date.getMinutes()) ? "" : pug_interp)) + "\u003C\u002Fspan\u003E\u003C\u002Fdiv\u003E";
 ;pug_debug_line = 11;pug_debug_filename = "components\u002Fchat\u002Fchat.jade";
 pug_html = pug_html + "\u003Cdiv class=\"message\"\u003E";
 ;pug_debug_line = 12;pug_debug_filename = "components\u002Fchat\u002Fchat.jade";
