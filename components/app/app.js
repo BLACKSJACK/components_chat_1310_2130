a03f0a79d7ce953ee9868c4f883a2c830fcf9cbc
Artsiom Mezin <iketari@gmail.com>
2017-12-08 20:29:02 +0000

    data from server

--- components/app/app.js
+++ components/app/app.js
@@ -20,18 +20,27 @@ export class App {
         });
 
         this.el.append(this.chat.el, this.form.el);
-        this.chat.add([
-            {
-                name: 'Ekaterina',
-                text: 'тогда и класс надо менять'
-            },
-            {
-                name: 'Ivan',
-                text: 'закрывающий div'
-            }
-        ]);
+
+        this._asyncInit();
+    }
+
+    async _asyncInit() {
+        let data = await this.getData();
+        let ms = await this._setChatData(data);
 
         this.render();
+        console.log(`Чат был запущен за ${ms} ms`);
+    }
+
+    _setChatData(data) {
+        return new Promise((resolve) => {
+            const ms = Math.round(Math.random() * 3000);
+
+            setTimeout(() => {
+                this.chat.add(data);
+                resolve(ms);
+            }, ms);
+        });
     }
 
     render() {
@@ -45,5 +54,27 @@ export class App {
             name: USER_NAME
         });
         this.render();
+    }
+
+    getData() {
+        return new Promise((resolve, reject) => {
+            let xhr = new XMLHttpRequest();
+            xhr.addEventListener('readystatechange', (event) => {
+                if (xhr.readyState !== 4) {
+                    return;
+                }
+
+                if (xhr.status === 502) {
+                    reject(xhr.status);
+                    return;
+                }
+
+                const data = JSON.parse(xhr.responseText);
+                resolve(data);
+            });
+
+            xhr.open('GET', '/data/chat.json', true);
+            xhr.send();
+        });
     }
 }
